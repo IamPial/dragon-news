c6601b1cc563f3ec2e8918354dd222871b51d382
Pial <pialname394@gmail.com>
2026-04-26 00:20:07 +0600

    added header page markup according to figma

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { format } from "date-fns";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="text-center py-8 space-y-2">
+      <Image
+        className="mx-auto"
+        src={logo}
+        width={500}
+        height={200}
+        alt="Logo"
+      />
+      <p className="text-gray-400 text-lg font-normal">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="font-medium text-xl text-gray-700">
+        {" "}
+        {format(new Date(), "EEEE,")}{" "}
+        <span className="text-gray-500">
+          {format(new Date(), " MMMM dd, yyyy")}
+        </span>{" "}
+      </p>
     </div>
   );
 };
