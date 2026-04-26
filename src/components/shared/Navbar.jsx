f5c1c6f1c6df8445c9cf5bcd9d8a131be7785bad
Pial <pialname394@gmail.com>
2026-04-26 17:40:16 +0600

    created navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import Link from "next/link";
+import userAvatar from "@/assets/user.png";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
+    <div className="grid grid-cols-3 justify-between items-center gap-4 container mx-auto my-5 ">
+      <div></div>
+      <ul className="flex items-center justify-center gap-5 text-gray-600 ">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/about-us"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/career"}>Career</Link>
+        </li>
+      </ul>
+      <div className="flex items-center justify-end gap-2 ">
+        <Image src={userAvatar} alt={"avatar"} width={40} height={40} />
+        <button className="bg-[#403F3F] btn text-white px-10 font-semibold text-xl rounded-none">
+          Login
+        </button>
+      </div>
     </div>
   );
 };
