8ec62d97c5a72ff3f5f12e54d3d8f5a993987e17
Pial <pialname394@gmail.com>
2026-04-27 12:17:12 +0600

    fetch data from categories by Id

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -9,16 +9,41 @@ const getCategories = async () => {
   return data.data;
 };
 
+const newsByGetCategoriesId = async (category_id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${category_id}`,
+  );
+  const data = await res.json();
+  return data.data;
+};
+
 export default async function Home() {
   const categories = await getCategories();
+  const news = await newsByGetCategoriesId("08");
+  console.log(news);
 
   return (
     <div className="container mx-auto grid grid-cols-12 gap-6 my-18">
       <div className=" col-span-3 rounded-md">
         <LeftSideBar categories={categories} activeId={null} />
       </div>
-      <div className="bg-indigo-100 col-span-6 rounded-md">
+      <div className=" col-span-6 rounded-md">
         <h2>Dragon News Home</h2>
+        <div>
+          {news.length === 0 ? (
+            <h2 className="text-5xl text-center my-20 font-bold text-gray-500">
+              No Data Found
+            </h2>
+          ) : (
+            news.map((n) => {
+              return (
+                <div key={n._id}>
+                  <h2 className="font-semibold ">Title: {n.title}</h2>
+                </div>
+              );
+            })
+          )}
+        </div>
       </div>
       <div className=" col-span-3 rounded-md">
         <RightSideBar />
