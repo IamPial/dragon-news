a3f10a2d7f1a75d87073bcdad11256f0d015e494
Pial <pialname394@gmail.com>
2026-04-28 10:14:41 +0600

    create a global loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Global Loading Page
+
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[80vh]">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+};
+
+export default LoadingPage;
