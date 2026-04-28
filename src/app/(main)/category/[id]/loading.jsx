c6ddd89e3d4d7fb97f8a5f61e374b2f00a5c1452
Pial <pialname394@gmail.com>
2026-04-28 10:15:22 +0600

    added loading spinner by category

---
+++ src/app/(main)/category/[id]/loading.jsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[80vh]">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+};
+
+export default LoadingPage;
